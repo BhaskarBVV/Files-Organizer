57995981c5297abd99e5408bd873bd20cafef7ad
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-02-14 02:29:47 +0530

    Create help.js

---
+++ Modular/Major Functions/help.js
@@ -0,0 +1,9 @@
+function helpfn() {
+    console.log(`List of all commands: 
+                1. tree cmd    - node Fo.jd tree <dirName>
+                2. organize cmd- node FO.js orgainse <dirName>
+                3. Help cmd    -  node FO.js help`);
+}
+module.exports={
+    helpkey  : helpfn
+}
